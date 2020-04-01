8a9f9cf5aa69e75dd6dbc297acec3a1de978ed93
Jason Berger <berge472@gmail.com>
2020-04-01 16:01:24 -0400

    filled out a lot of details

---
+++ gatt_server.c
@@ -0,0 +1,63 @@
+/**
+ * @file gatt_server.c
+ * @author Jason Berger
+ * @brief Abstract/Generic Gatt server.  
+ * @version 0.1
+ * @date 2020-02-21
+ * 
+ * 
+ */
+
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdlib.h>
+#include <string.h>
+#include "gatt_server.h"
+
+
+mrt_status_t gatt_init_svc(mrt_gatt_svc_t* svc, uuid_type_e uuidType, const uint8_t* arrUuid, uint16_t charCount, mrt_gatt_svc_callback cbEvent)
+{
+     /* Set UUID */
+    svc->mUuid.mType = uuidType;
+    if(uuidType == e16Bit)
+    {   
+        memcpy((uint8_t*)&svc->mUuid.m16Bit, arrUuid, 2);
+    }
+    else
+    {
+        memcpy((uint8_t*)&svc->mUuid.m128Bit, arrUuid, 16);
+    }
+
+    /* malloc memory for characteristic descriptors */
+    svc->mCharCount = charCount;
+    svc->mChars = (mrt_gatt_char_t*)malloc(sizeof(mrt_gatt_char_t) * charCount);
+    svc->cbEvent = cbEvent;
+    
+
+    return MRT_STATUS_OK;
+}
+
+mrt_status_t gatt_init_char(mrt_gatt_char_t* chr, uuid_type_e uuidType, const uint8_t* arrUuid, uint16_t size, uint8_t props, mrt_gatt_char_callback cbEvent  )
+{
+
+    /* Set UUID */
+    chr->mUuid.mType = uuidType;
+    if(uuidType == e16Bit)
+    {   
+        memcpy((uint8_t*)&chr->mUuid.m16Bit, arrUuid, 2);
+    }
+    else
+    {
+        memcpy((uint8_t*)&chr->mUuid.m128Bit, arrUuid, 16);
+    }
+
+    chr->mSize = size;
+    chr->mProps = props;
+    chr->cbEvent = cbEvent;
+    chr->mNotificationsEnable = false;
+
+    return MRT_STATUS_OK;
+}
+
+
+
--- gatt_server.h
+++ gatt_server.h
@@ -7,20 +7,28 @@
  * 
  * 
  */
-#pragma once
+
+#ifndef GATT_SERVER_H_
+#define GATT_SERVER_H_
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-#define GATT_PERM_R    0x01
-#define GATT_PERM_W    0x02
-#define GATT_PERM_N    0x04
-#define GATT_PERM_RW   0x03
-#define GATT_PERM_RWN  0x07
+#define MRT_GATT_PROP_NONE                           0x00
+#define MRT_GATT_PROP_BROADCAST                      0x01
+#define MRT_GATT_PROP_READ                           0x02
+#define MRT_GATT_PROP_WRITE_WITHOUT_RESP             0x04
+#define MRT_GATT_PROP_WRITE                          0x08
+#define MRT_GATT_PROP_NOTIFY                         0x10
+#define MRT_GATT_PROP_INDICATE                       0x20
+#define MRT_GATT_PROP_SIGNED_WRITE                   0x40
+#define MRT_GATT_PROP_EXT                            0x80
 
 /* Includes ------------------------------------------------------------------*/
+#define MRT_GATT_MODULE_ENABLED
 #include "Platforms/Common/mrt_platform.h"
+
 
 /* Exported types ------------------------------------------------------------*/
 
@@ -31,42 +39,69 @@ typedef enum{
     e128Bit
 }uuid_type_e;
 
-//BLE characteristic definition
-typedef struct mrt_ble_char_t mrt_ble_char_t;                             //forward declare for self referencing callback
-typedef mrt_ble_char_t (*mrt_ble_char_t)(mrt_ble_char_t* svc); 
+typedef enum{
+    GATT_EVT_CHAR_NONE     = 0x00,
+    GATT_EVT_CHAR_WRITE    = 0x01,
+    GATT_EVT_CHAR_READ     = 0x02,
+    GATT_EVT_NOTIFY_ENABLE = 0x04,
+    GATT_EVT_NOTIFY_DISABLE = 0x08
+}mrt_gatt_evt_type_e;
 
-//UUID struct
+/* UUID struct */
 typedef struct{
     union {
         uint16_t m16Bit;            //16bit uuid
         uint8_t m128Bit[16];        //128bit uuid
     };
     uuid_type_e mType;              //type 16bit or 128bit
-} mrt_ble_uuid_t;
+} mrt_gatt_uuid_t;
 
-//Ble Service definition
-typedef struct mrt_ble_svc_t mrt_ble_svc_t;                             //forward declare for self referencing callback
-typedef mrt_status_t (*mrt_ble_svc_callback)(mrt_ble_svc_t* svc); 
 
-typedef mrt_ble_char_t{
-    mrt_ble_uuid_t mUuid;           //UUID of Characteristic
+/* Forward Declare for self referencing function pointers */
+typedef struct mrt_gatt_svc_t mrt_gatt_svc_t;                             //forward declare for self referencing callback
+typedef struct mrt_gatt_char_t mrt_gatt_char_t;                             //forward declare for self referencing callback
+
+
+/* gatt Event struct*/
+typedef struct{
+    mrt_gatt_evt_type_e mType;   /*Type of event*/
+    mrt_gatt_char_t* mChar;      /*Characteristic for event*/
+    struct{
+        uint8_t* data;          /*ptr to data for writes (or notify on client)*/
+        int len;                /* len of data in bytes*/
+    }mData;
+} mrt_gatt_evt_t;
+
+typedef mrt_status_t (*mrt_gatt_svc_callback)(mrt_gatt_svc_t* svc, mrt_gatt_evt_t* event); 
+typedef mrt_status_t (*mrt_gatt_char_callback)(mrt_gatt_evt_t* event); 
+
+/* gatt characteristic descriptor */
+struct mrt_gatt_char_t{
+    mrt_gatt_uuid_t mUuid;          //UUID of Characteristic
     uint16_t mSize;                 //size of Characteristic
-    void* mHandle;                  //Handle of Characteristic
-    uint8_t mPerm;                  //Permissions of characteristic
-} ;
+    mrt_gatt_chr_handle_t mHandle;      //Handle of Characteristic
+    uint8_t mProps;                 //Permissions of characteristic
+    bool mNotificationsEnable;      //Indicates if Notifications are enabled in CCCD
+    mrt_gatt_char_callback cbEvent; //characteristic event callback
+};
 
-
-typedef mrt_ble_svc_t{
-    mrt_ble_char_t* mChars;         //Array of characteristics   
+/* gatt Service descriptor */
+struct mrt_gatt_svc_t{
+    mrt_gatt_uuid_t mUuid;          //UUID of Service
+    mrt_gatt_svc_handle_t  mHandle;
+    mrt_gatt_char_t* mChars;         //Array of characteristics   
     uint16_t mCharCount;            //Number of characterestics
-    mrt_ble_svc_callback cbWrite;   //characteristic write callback
-} ;
+    mrt_gatt_svc_callback cbEvent;   //Service event callback
+};
 
 /* Exported constants --------------------------------------------------------*/
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
+mrt_status_t gatt_init_svc(mrt_gatt_svc_t* svc, uuid_type_e uuidType, const uint8_t* arrUuid, uint16_t charCount, mrt_gatt_svc_callback cbEvent);
+mrt_status_t gatt_init_char(mrt_gatt_char_t* chr, uuid_type_e uuidType, const uint8_t* arrUuid, uint16_t size, uint8_t props, mrt_gatt_char_callback cbEvent );
 
 
 #ifdef __cplusplus
 }
 #endif
+#endif /*GATT_SERVER_H_*/
